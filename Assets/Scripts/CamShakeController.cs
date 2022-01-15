e340574916ff1cc41c810e8623eedfb26df5c46b
Mustafa Salih Oğuz <mustafasalihoguz@gmail.com>
2022-01-15 14:01:02 +0300

    Add post processing

--- Assets/Scripts/CamShakeController.cs
+++ Assets/Scripts/CamShakeController.cs
@@ -4,15 +4,20 @@ using System.Collections.Generic;
 using Cinemachine;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class CamShakeController : MonoBehaviour
 {
     public static CamShakeController Instance;
 
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private PostProcessVolume postProcessVolume;
     
     private CinemachineBasicMultiChannelPerlin _camShake;
-
+    private Bloom _bloom;
+    private ChromaticAberration _chromaticAberration;
+    private Vignette _vignette;
+    
     private void Awake()
     {
         Instance = this;
@@ -21,16 +26,33 @@ public class CamShakeController : MonoBehaviour
     private void Start()
     {
         _camShake = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        postProcessVolume.profile.TryGetSettings(out _bloom);
+        postProcessVolume.profile.TryGetSettings(out _chromaticAberration);
+        postProcessVolume.profile.TryGetSettings(out _vignette);
     }
 
     public void CamShake()
     {
         DOTween.Kill("CamShake");
-        DOTween.To(() => _camShake.m_AmplitudeGain, x => _camShake.m_AmplitudeGain = x, 
-            2, 0.2f).SetId("CamShake").OnComplete(() =>
+        DOTween.To(() => _camShake.m_AmplitudeGain, x => _camShake.m_AmplitudeGain = x, 2, 0.2f).SetId("CamShake").OnComplete(() =>
         {
-            DOTween.To(() => _camShake.m_AmplitudeGain, x => _camShake.m_AmplitudeGain = x,
-                0, 0.1f).SetId("CamShake");
+            DOTween.To(() => _camShake.m_AmplitudeGain, x => _camShake.m_AmplitudeGain = x, 0, 0.1f).SetId("CamShake");
         });
+
+        DOTween.To(() => _bloom.intensity.value, x => _bloom.intensity.value = x, 4, 0.2f).SetId("CamShake").OnComplete(() =>
+        {
+            DOTween.To(() => _bloom.intensity.value, x => _bloom.intensity.value = x, 0, 0.1f).SetId("CamShake");
+        });
+        
+        DOTween.To(() => _chromaticAberration.intensity.value, x => _chromaticAberration.intensity.value = x, 1, 0.2f).SetId("CamShake").OnComplete(() =>
+        {
+            DOTween.To(() => _chromaticAberration.intensity.value, x => _chromaticAberration.intensity.value = x, 0, 0.1f).SetId("CamShake");
+        });
+        
+        DOTween.To(() => _vignette.intensity.value, x => _vignette.intensity.value = x, 0.4f, 0.2f).SetId("CamShake").OnComplete(() =>
+        {
+            DOTween.To(() => _vignette.intensity.value, x => _vignette.intensity.value = x, 0, 0.1f).SetId("CamShake");
+        });
+
     }
 }
