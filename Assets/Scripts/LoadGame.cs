3f7f1ee84a916aa0809cee5a25d37163f0248ede
Mustafa Salih Oğuz <mustafasalihoguz@gmail.com>
2022-01-15 14:41:19 +0300

    Add toon shader

--- Assets/Scripts/LoadGame.cs
+++ Assets/Scripts/LoadGame.cs
@@ -7,6 +7,7 @@ public class LoadGame : MonoBehaviour
 {
     void Start()
     {
+        Application.targetFrameRate = 60;
         SceneManager.LoadScene("Level" + Mathf.Min(PlayerPrefs.GetInt("Level"), 2));
     }
 }
